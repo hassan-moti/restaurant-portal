cb65e14c8a732d6dbddfe4811883dbf81a57fe13
hassan motiwala <muhammad.hassanmoti@gmail.com>
2022-06-14 16:17:20 +0500

    some changes in api

--- src/app/components/MainMenu/product-list/product-list.component.ts
+++ src/app/components/MainMenu/product-list/product-list.component.ts
@@ -4,6 +4,9 @@ import { Product } from '../../interface';
 import { ProductListService } from '../../product-list.service';
 import { NzModalService, NzModalRef } from 'ng-zorro-antd/modal';
 
+import {NzUploadChangeParam} from 'ng-zorro-antd/upload';
+import {NzMessageService} from 'ng-zorro-antd/message';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -23,7 +26,8 @@ export class ProductListComponent implements OnInit {
     private productServices: ProductListService,
     private fb: FormBuilder,
     private modal: NzModalService,
-    private viewContainerRef: ViewContainerRef
+    private viewContainerRef: ViewContainerRef,
+    private msg: NzMessageService
   ) { }
 
   showModal(product?: Product): void {
@@ -33,12 +37,12 @@ export class ProductListComponent implements OnInit {
       this.productForm.patchValue({
         productName: this.selectedProduct.productName,
         productPrice: this.selectedProduct.price,
-        imageTitle:this.selectedProduct.imageTitle
+        imageTitle: this.selectedProduct.imageTitle
       });
 
     }
   }
-  handleSumbit(selectedProduct:any): void {
+  handleSubmit(selectedProduct: any): void {
     let proudctToBeUpdated;
     for (const prod of this.products) {
       if (prod.productName === this.selectedProduct?.productName) {
@@ -52,21 +56,19 @@ export class ProductListComponent implements OnInit {
       imageTitle: this.productForm.get('imageTitle')?.value
       // imageTitle: proudctToBeUpdated.imageTitle
     }
+
     const index = this.products.indexOf(proudctToBeUpdated);
     if (index !== -1) {
       this.products[index] = updatedProduct;
     }
-
-    if (selectedProduct===undefined){
-          this.productServices.addMenu(updatedProduct).subscribe((result)=>
-    console.warn(" added mubarak ho"));
+console.log(selectedProduct);
+    if (selectedProduct == null){
+          this.productServices.addMenu(updatedProduct).subscribe((result)=>{this.productServices.getmenu().subscribe(data => this.products = data);});
     }
 
     else{
     this.productServices.updateMenu(proudctToBeUpdated.productName,updatedProduct).
-    subscribe((result)=>console.log(result,"data updated mubarak ho "));
-
-    this.productServices.getmenu().subscribe(data => this.products = data);
+    subscribe((result)=>{this.productServices.getmenu().subscribe(data => this.products = data);});
     }
     this.isVisible = false;
     this.selectedProduct = undefined;
@@ -85,13 +87,11 @@ export class ProductListComponent implements OnInit {
     this.productForm.patchValue({
       productName: null,
       productPrice: null,
-      imageTitle:null
+      imageTitle: null
     });
     this.isVisible = true;
   }
-  //   this.productServices.addMenu(updatedProduct).subscribe((result)=>
-  //   console.warn(result," added mubarak ho"));
-  showDeleteConfirm(product:string): void {
+  showDeleteConfirm(product: string): void {
     this.modal.confirm({
       nzTitle: 'Are you sure delete this Item?',
       nzContent: '<b style="color: red;">some description</b>',
@@ -99,13 +99,25 @@ export class ProductListComponent implements OnInit {
       nzOkType: 'primary',
       nzOkDanger: true,
       nzOnOk: () => {
-      this.productServices.deleteMenu(product).subscribe();
-      this.productServices.getmenu().subscribe(data => this.products = data);
+        this.productServices.deleteMenu(product).subscribe((result)=>{this.productServices.getmenu().subscribe(data => this.products = data);});
       },
       nzCancelText: 'No',
       nzOnCancel: () => console.log('Cancel')
     });
   }
+
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
+  }
+
+
   ngOnInit(): void {
     this.productServices.getmenu().subscribe(data => this.products = data);
     console.log(this.products);
